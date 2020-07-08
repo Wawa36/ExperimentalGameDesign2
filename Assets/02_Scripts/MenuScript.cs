a5586a928cb999754c82c0e788c2ffde49999a1f
Wawa36 <felixszczesny@yahoo.fr>
2020-07-08 16:23:10 +0200

    [added] UI Start/Replay/Win prefab with event system

---
+++ Assets/02_Scripts/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    private void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void StartTime()
+    {
+        Time.timeScale = 1;
+    }
+}
