77f3565bd30eb80847c76369e874c3c72d89a885
Lila <kasamatsulila@gmail.com>
2020-07-09 13:55:34 +0200

    [fixed] game instantly jumping to replay screen without starting
    had to disable objectmanager.cs to disable event call

--- Assets/02_Scripts/ObjectManager.cs
+++ Assets/02_Scripts/ObjectManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class ObjectManager : MonoBehaviour
 {
     ObjectPlayerLine[] objects;
+    bool allAreVisible;
+
+    public GameEvent winningCondition;
+
 
     // Start is called before the first frame update
     void Start()
@@ -12,12 +16,26 @@ public class ObjectManager : MonoBehaviour
         objects = Object.FindObjectsOfType<ObjectPlayerLine>();
     }
 
+
+
     // Update is called once per frame
     void Update()
     {
+        allAreVisible = true;
         foreach(ObjectPlayerLine obj in objects){
+            if(!(obj.isTotallyVisible && !obj.isIntersected))
+            {
+                allAreVisible = false;
+            }
             obj.isIntersected = false;
         }
+
+        if (allAreVisible)
+        {
+            Debug.Log("WON");
+            winningCondition.Raise();
+        }
+
         foreach(ObjectPlayerLine obj in objects)
         {
             obj.CheckForVisibility();
