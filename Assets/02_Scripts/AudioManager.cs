f772be86029d0fb7f28f186e2c93f70fd80e8490
Julian <kontakt@julianhellmann.de>
2020-07-13 13:02:39 +0200

    Audiomanager implementiert

---
+++ Assets/02_Scripts/AudioManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = this.gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        
+
+        //Play("Stone");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //    Stop("Stone");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s != null)
+            s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s != null)
+            s.source.Stop();
+    }
+}
